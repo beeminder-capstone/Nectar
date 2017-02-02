1e494f1079d13f8f2e241af9e2237b58c0729d1e
naxt25 <naxt@pdx.edu>
2017-02-01 19:28:49 -0800

    fixed type in Gemfile

--- app/metrics/facebook/total_posts_per_day.rb
+++ app/metrics/facebook/total_posts_per_day.rb
@@ -1,8 +1,15 @@
+
 PROVIDERS.fetch(:facebook).register_metric :total_posts_per_day do |metric|
   metric.description = "Sum of POSTS made by user per day"
   metric.title = "POSTS backlog"
 
   metric.block = proc do |adapter|
-    #insert code here
+    wall = @client.get_connections("me", "feed", fields: ['id'])
+    wall.each {|p| get_my_days_posts(p)}
+    next_wall = wall.next_page
+  end
+
+  def get_my_days_posts(post)
+    puts(post.id)
   end
 end
