c8f258d78971cf0bfd6baed389d2b599c4f27b40
earthshine0 <earthshinelynx@gmail.com>
2017-02-10 16:10:23 -0800

    Updated totalPostsPerDay metric

--- app/metrics/facebook/total_posts_per_day.rb
+++ app/metrics/facebook/total_posts_per_day.rb
@@ -4,12 +4,17 @@ PROVIDERS.fetch(:facebook).register_metric :total_posts_per_day do |metric|
   metric.title = "POSTS backlog"
 
   metric.block = proc do |adapter|
-    wall = @client.get_connections("me", "feed", fields: ['id'])
-    wall.each {|p| get_my_days_posts(p)}
-    next_wall = wall.next_page
+    wall = @client.get_connections("me", "feed", fields: %w(id from created_time))
+    # me_id = @client.get_object("me")/{profile=>id}
+    @client.put_connection("me", "feed", message: "hi all")
+    me = @client.get_object("me")
+    wall.each {|p| get_my_days_posts(p, me)}
+    # next_wall = wall.next_page
   end
 
-  def get_my_days_posts(post)
-    puts(post.id)
+  def get_my_days_posts(post, me)
+    if post.from.equal?(me)
+      puts 'it works'
+    end
   end
 end
