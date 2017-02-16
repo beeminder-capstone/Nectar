a987fc3154492dfd6300aedd7f04993a9d68d120
earthshine0 <earthshinelynx@gmail.com>
2017-02-15 21:49:46 -0800

    • Modified facebook_adapter.rb to properly define client. Added the following functions:       • fetch_wall       • fetch_me       • fetch_my_days_posts
    •  Modified total_posts_per_day.rb to add a datapoint with user's total posts

--- app/metrics/facebook/total_posts_per_day.rb
+++ app/metrics/facebook/total_posts_per_day.rb
@@ -1,20 +1,46 @@
 
 PROVIDERS.fetch(:facebook).register_metric :total_posts_per_day do |metric|
   metric.description = "Sum of POSTS made by user per day"
-  metric.title = "POSTS backlog"
+  metric.title = "Facebook posts per day"
 
   metric.block = proc do |adapter|
-    wall = @client.get_connections("me", "feed", fields: %w(id from created_time))
+    #Get the user's wall from facebook (first 100 items)
+    # wall = @client.get_connections("me", "feed", fields: %w(id from created_time))
+    # adapter.create_post
+    # adapter.test_adapter
+    # adapter.fetch_wall
+    # puts adapter.fetch_me
+    # puts adapter.fetch_wall
+    # puts adapter.fetch_wall.count(&:id)
+    puts adapter.fetch_my_days_posts
+
+
+    Datapoint.new(value: adapter.fetch_wall.count(&:id))
+    # wall = adapter.fetch_wall
+
+    # Use this line if comparing profiles directly doesn't work
     # me_id = @client.get_object("me")/{profile=>id}
-    @client.put_connection("me", "feed", message: "hi all")
-    me = @client.get_object("me")
-    wall.each {|p| get_my_days_posts(p, me)}
+
+
+    #Get the user's profile
+    # me = @client.get_object("me")
+
+    # wall.each {|p| get_my_days_posts(p, me)}
+
+    #We need to get the posts that are the users
+    # Where the creator of the post is equal to the user's profile
+
+
+    # Only need this line if user is posting 100+ a day
     # next_wall = wall.next_page
+
+  # end.map do |ts, value|
+  #   Datapoint.new(unique: true, timestamp: ts, value: value)
+  # end.sort_by(&:timestamp)
   end
 
-  def get_my_days_posts(post, me)
-    if post.from.equal?(me)
-      puts 'it works'
-    end
-  end
+
+
 end
+
+
