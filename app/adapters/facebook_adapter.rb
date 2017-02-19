b221b0ca3390241d562527255429b67dd842b668
Rachael Johnson <rhatchet@users.noreply.github.com>
2017-02-19 12:01:29 -0800

    Facebook integration (#2)
    * /Facebook:
    
    * • Modified facebook_adapter.rb to properly define client. Added the following functions:
          • fetch_wall
          • fetch_me
          • fetch_my_days_posts
    
    •  Modified total_posts_per_day.rb to add a datapoint with user's total posts
    
    * • Modified facebook_adapter.rb to properly define client. Added the following functions:
          • fetch_wall
          • fetch_me
          • fetch_my_days_posts
    
    •  Modified total_posts_per_day.rb to add a datapoint with user's total posts
    
    * Update .travis.yml
    
    * Finished credentials page.
    
    * /Facebook:
    
    * Removed changes to add goals page.
    
    * • Modified facebook_adapter.rb to properly define client. Added the following functions:
          • fetch_wall
          • fetch_me
          • fetch_my_days_posts
    
    •  Modified total_posts_per_day.rb to add a datapoint with user's total posts
    
    * • Modified facebook_adapter.rb to properly define client. Added the following functions:
          • fetch_wall
          • fetch_me
          • fetch_my_days_posts
    
    •  Modified total_posts_per_day.rb to add a datapoint with user's total posts
    
    * Updated credentials page.
    
    * Updated Google config.
    
    * fixed facebook_adapter

--- app/adapters/facebook_adapter.rb
+++ app/adapters/facebook_adapter.rb
@@ -1,4 +1,7 @@
+require 'koala'
+
 class FacebookAdapter < BaseAdapter
+
   class << self
     def required_keys
       %i(token)
@@ -16,11 +19,36 @@ class FacebookAdapter < BaseAdapter
       "Facebook"
     end
   end
+
+
+  def client
+    @client = Koala::Facebook::API.new(access_token)
+  end
+
+  def fetch_wall()
+    client.get_connection('me', 'feed', fields: %w(id from created_time))
+  end
+
+
+  def fetch_me
+    client.get_object("me")
+  end
+
+
+  #returns posts created by the user
+  def fetch_my_days_posts()
+    wall = fetch_wall
+    sum = 0
+    wall.each do |post|
+       sum += (post.from === fetch_me && !(DateTime.jisx0301(post.created_time) < Date.today)) ? 1 : 0
+    end
+    sum
+  end
+
+  def create_post
+    #Test message to see if this works
+    client.put_connections("me", "feed", message: "hi all")
+  end
+
 end
 
-def client
-  Koala::Facebook::Api.new(
-      oath_access_token: Rails.application.secrets.facebook_provider_key,
-      app_secret: Rails.application.secrets.facebook_secret_key
-  )
-end
--- app/metrics/facebook/total_logins_per_day.rb
+++ app/metrics/facebook/total_logins_per_day.rb
@@ -5,4 +5,7 @@ PROVIDERS.fetch(:facebook).register_metric :total_logins_per_day do |metric|
   metric.block = proc do |adapter|
     #insert code here
   end
+
+
+
 end
