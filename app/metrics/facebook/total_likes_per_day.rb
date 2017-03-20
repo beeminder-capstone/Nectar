696534f5f48c8cd07a23f07066edf6fcc3dcc026
earthshine0 <earthshinelynx@gmail.com>
2017-03-19 20:17:54 -0700

    fixed facebook_adapter

--- app/metrics/facebook/total_likes_per_day.rb
+++
@@ -1,8 +0,0 @@
-PROVIDERS.fetch(:facebook).register_metric :total_likes_per_day do |metric|
-  metric.description = "Sum of LIKES made by user per day"
-  metric.title = "LIKES backlog"
-
-  metric.block = proc do |adapter|
-    #insert code here
-  end
-end
