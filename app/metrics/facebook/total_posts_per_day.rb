6f364c6c46f65982025bfa931a1f8f9313c89e60
earthshine0 <earthshinelynx@gmail.com>
2017-03-19 20:11:27 -0700

    fixed facebook_adapter

--- app/metrics/facebook/total_posts_per_day.rb
+++ app/metrics/facebook/total_posts_per_day.rb
@@ -5,13 +5,7 @@ PROVIDERS.fetch(:facebook).register_metric :total_posts_per_day do |metric|
 
   metric.block = proc do |adapter|
     Datapoint.new(value: adapter.fetch_my_days_posts)
-
-  # end.map do |ts, value|
-  #   Datapoint.new(unique: true, timestamp: ts, value: value)
-  # end.sort_by(&:timestamp)
   end
-
-
 
 end
 
